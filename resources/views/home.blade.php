8c3554f176849651ac86294d7265688cde78b72a
ReDMallard <marco@titori.it>
2024-01-23 02:20:18 +0100

    richiama elementi con foreach anziche vardump

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 <h1>film</h1>
-@php
-    var_dump($movies);
-@endphp
+<div class="container">
+    @foreach ($movies as $movie)
+    <div class="card">
+        <h3>{{$movie->title}}</h3>
+        <h5>{{$movie->original_title}}</h5>
+        <p>{{$movie->nationality}}</p>
+        <p>{{$movie->vote}}</p>
+
+    </div>   
+    @endforeach
+</div>
 
 @endsection
